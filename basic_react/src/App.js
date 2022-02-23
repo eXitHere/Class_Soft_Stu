5950dd75d7960c100940549c603412d0aaffcc46
littlelunar <lunar.inthemoonllight@gmail.com>
2022-02-23 12:27:26 +0700

    add user information

--- basic_react/src/App.js
+++ basic_react/src/App.js
@@ -3,19 +3,37 @@ import Profile from './components/profile';
 function App() {
 	const userDate = [
 		{
-			name: 'Thana XXX',
-			studentId: '62010381',
+			name: 'Thanakorn Chancherngpanich',
+			studentId: '62010336',
 			text: 'asdsad',
-			profile: 'https://avatars.githubusercontent.com/u/50941709?v=4',
+			profile: 'https://avatars.githubusercontent.com/u/57614928?v=4',
 		},
 		{
-			name: 'Thana XXX',
+			name: 'Thanadol Sinananvanich',
+			studentId: '62010345',
+			text: 'asdasdsad',
+			profile: 'https://avatars.githubusercontent.com/u/40865904?v=4',
+		},
+		{
+			name: 'Thana Tingprasom',
 			studentId: '62010381',
+			text: 'asdsad',
+			profile: 'https://avatars.githubusercontent.com/u/39361943?v=4',
+		},
+		{
+			name: 'Prachya Thongsan',
+			studentId: '62010533',
+			text: 'asdasdsad',
+			profile: 'https://avatars.githubusercontent.com/u/70775537?v=4',
+		},
+		{
+			name: 'Paratthakorn Sribunyong',
+			studentId: '62010535',
 			text: 'asdasdsad',
 			profile: 'https://avatars.githubusercontent.com/u/50941709?v=4',
-		},
+		}
 	];
-
+	
 	return (
 		<div className=''>
 			{userDate.map((e, idx) => (
